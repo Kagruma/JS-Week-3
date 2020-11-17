5758428aa2dd552143c4dd2785ee8e3815d6ce68
Kagruma <nguyene375@gmail.com>
2020-11-17 12:07:07 -0500

    finished easy and medium challenges

---
+++ Easy/scripts.js
@@ -0,0 +1,10 @@
+function run(){
+    console.log("Today's exercise: running");
+}
+function swim(){
+    console.log("Today's exercise: swimming");
+}
+run();
+swim();
+
+
---
+++ Medium/scripts.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function sharepizza(people) {
+return 8 / people;
+}
+
+const slicesPizza = sharepizza(2);
+console.log("Each person gets " + slicesPizza +" slices of pizza!");
