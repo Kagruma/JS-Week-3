25e58d62a7ab9441b322dbb4778915afbad5fd19
Kagruma <nguyene375@gmail.com>
2020-11-23 17:28:56 -0500

    Revert to old commits

--- Easy/scripts.js
+++ Easy/scripts.js
@@ -1,2 +1,10 @@
-let arrayAVG = (array) => array.reduce((a, b) => a + b) / array.length;
-console.log(arrayAVG([2100,330,530,100,270]));
+function run(){
+    console.log("Today's exercise: running");
+}
+function swim(){
+    console.log("Today's exercise: swimming");
+}
+run();
+swim();
+
+
--- Very Hard/scripts.js
+++ Very Hard/scripts.js
@@ -1,26 +1,64 @@
-<<<<<<< HEAD
+class Person {
 
-'use strict';
-
-function countcoins(t, o) {
-
-    
-    var targetsLength = t + 1;
-    var operandsLength = o.length;
-    t = [1];
- 
-    for (var a = 0; a < operandsLength; a++) {
-        for (var b = 1; b < targetsLength; b++) {
- 
-            // initialise undefined target
-            t[b] = t[b] ? t[b] : 0;
- 
-            // accumulate target + operand ways
-            t[b] += (b < o[a]) ? 0 : t[b - o[a]];
-        }
+    constructor(name, job, age){
+        this.name = name;
+        this.job = job;
+        this.age = age;
     }
- 
-    return t[targetsLength - 1];
+
+    exercise(){
+        console.log('generic exercise')
+    }
+
+    fetchJob(){
+        console.log(`${this.name} is a ${this.job}`);
+    }
+
 }
-=======
->>>>>>> parent of 79785b0... finished all challenges
+
+var p1 = new Person("Alex", "Architect", 142)
+
+p1.exercise();
+p1.fetchJob();
+
+class Programmer extends Person{
+    constructor(name, job, age, languages){
+    super(name,job,age);
+    this.languages = languages;
+    this.busy = true; }
+
+
+        completeNewTask(){
+            this.busy = false;
+        }
+
+
+        acceptNewTask(){
+            this.busy = true;
+        }
+
+        offerNewTask(){
+            if(this.busy){
+                console.log(`${this.name} can't accept and new tasks at the moment.`);
+            } else {
+                console.log(`${this,name} would love to take on a new task!`);
+            }
+        }
+
+        learnLanguages(lang){
+            this.languages.push(lang)
+        }
+
+        listLanguages(){
+            for(let x = 0; x<this.languages.length; x++){
+                console.log(this.languages[x]);
+            }
+        }
+
+}
+
+var p1 = new Person("Eve","Programmer", 19);
+var c1 = new Programmer("Eve","Junior Dev", 19, ['Java','LUA', 'HTML', 'CSS'])
+c1.fetchJob();
+
+c1.completeNewTask();
